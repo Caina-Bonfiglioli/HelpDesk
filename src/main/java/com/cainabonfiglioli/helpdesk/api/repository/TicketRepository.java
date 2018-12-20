e3b9254f8315021082d56fb278b878e1304a9f62
Cainã Bonfiglioli <caina.bonfiglioli@gmail.com>
2018-12-20 15:54:52 -0200

    modificado o TicketRepository e criado o TicketService e o TicketServiceImpl

--- src/main/java/com/cainabonfiglioli/helpdesk/api/repository/TicketRepository.java
+++ src/main/java/com/cainabonfiglioli/helpdesk/api/repository/TicketRepository.java
@@ -8,17 +8,17 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.cainabonfiglioli.helpdesk.api.entity.Ticket;
 
 public interface TicketRepository extends MongoRepository<Ticket, String>{
-	
+
 	Page<Ticket> findByUserIdOrderByDateDesc(Pageable pages, String userId);
-	
+
 	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityOrderByDateDesc(
 			String title, String status, String priority, Pageable pages);
-	
+
 	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAndUserIdOrderByDateDesc(
 			String title, String status, String priority, Pageable pages);
-	
+
 	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAndAssignedUserIdOrderByDateDesc(
 			String title, String status, String priority, Pageable pages);
-	
+
 	Page<Ticket> findByNumber(Integer number, Pageable pages);
 }
